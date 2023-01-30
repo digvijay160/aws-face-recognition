30a50c0d46429d2b7c29bbe16a251ac81f3bb345
digvijay160 <digvijaychauhan.160@gmail.com>
2023-01-30 12:08:48 +0530

    new images added

--- add_images.py
+++ add_images.py
@@ -4,22 +4,22 @@ s3=boto3.resource('s3')
 
 #list of images fro indexing
 images=[
-        ('dhoni1.jpg','MS Dhoni'),
-        # ('dhoni2.jpeg','MS Dhoni'),
-        # ('dhoni3.jpg','MS Dhoni'),
-        ('dhoni4.jpg','MS Dhoni'),
-        ('cr1.jpg','Cristiano Ronaldo'),
-        ('cr2.jpg','Cristiano Ronaldo'),
-        # ('cr3.jpg','Cristiano Ronaldo'),
-        # ('cr4.jpg','Cristiano Ronaldo'),
-        # ('jlaw1.jpg','Jennifer Lawrence'),
-        ('jlaw2.jpg','Jennifer Lawrence'),
-        ('jlaw3.jpg','Jennifer Lawrence'),
-        # ('jlaw4.jpg','Jennifer Lawrence'),
-        ('nooyi1.jpg','Indra Nooyi'),
-        # ('nooyi2.jpg','Indra Nooyi'),
-        # ('nooyi3.jpg','Indra Nooyi'),
-        ('nooyi4.jpg','Indra Nooyi')
+        # ('dhoni1.jpg','MS Dhoni'),
+        ('dhoni2.jpeg','MS Dhoni'),
+        ('dhoni3.jpg','MS Dhoni'),
+        # ('dhoni4.jpg','MS Dhoni'),
+        # ('cr1.jpg','Cristiano Ronaldo'),
+        # ('cr2.jpg','Cristiano Ronaldo'),
+        ('cr3.jpg','Cristiano Ronaldo'),
+        ('cr4.jpg','Cristiano Ronaldo'),
+        ('jlaw1.jpg','Jennifer Lawrence'),
+        # ('jlaw2.jpg','Jennifer Lawrence'),
+        # ('jlaw3.jpg','Jennifer Lawrence'),
+        ('jlaw4.jpg','Jennifer Lawrence'),
+        # ('nooyi1.jpg','Indra Nooyi'),
+        ('nooyi2.jpg','Indra Nooyi'),
+        ('nooyi3.jpg','Indra Nooyi'),
+        # ('nooyi4.jpg','Indra Nooyi')
     ]
 
 #iterate through images to upload to s3
